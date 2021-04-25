7331f417097962369dc761a7bffd5f0f06d32f11
neeraja <neeraja_paladugu@outlook.com>
2021-04-25 17:29:20 -0400

    competed week3 Assignments

--- src/week3/AddCommasToNumberStrings.java
+++ src/week3/AddCommasToNumberStrings.java
@@ -1,11 +1,37 @@
 package week3;
 
+import java.text.DecimalFormat;
+
 import acm.program.ConsoleProgram;
 
 public class AddCommasToNumberStrings extends ConsoleProgram {
 	
 	public void run() {
+		while (true) {
+			 String digits = readLine("Enter a numeric string: ");
+			 if (digits.length() == 0) break;
+			 println(addCommasToNumericString(digits));
+			 }
 
+
+	}
+
+	private String addCommasToNumericString(String digits) {
+		// TODO Auto-generated method stub
+		int len = digits.length();
+		String results = "";
+		int num = 0;
+		for (int i = len -1; i >=0; i--)//1000
+		{
+			results = digits.charAt(i) + results;
+			num++;
+			// this  will add comma after every 3 digits
+			if((num % 3 == 0) && (i>0)) {
+				results = "," + results;
+			}
+			
+		}
+		return results;
 	}
 	
 }
--- src/week3/DeleteCharactersFromString.java
+++ src/week3/DeleteCharactersFromString.java
@@ -5,7 +5,30 @@ import acm.program.ConsoleProgram;
 public class DeleteCharactersFromString extends ConsoleProgram {
 	
 	public void run() {
-
+		String str = "How are you doing today?";
+		System.out.println(removeAllOccurrences(str,  't' ));
 	}
+	public String removeAllOccurrences(String str, char ch) {
+		char y[] = str.toCharArray();
+		int size = str.length();
+		
+		int i = 0;
+		String res = "";
+		
+		while(i !=size)
+			if(y[i] != ch) {
+				
+				res=res +y[i];
+				i++;
+			}
+			else {
+				i++;
+			}
+			return res;
+		
+		//return str.substring(0, ch) + str.substring(ch + 1);
+		
+	}
+	
 	
 }
